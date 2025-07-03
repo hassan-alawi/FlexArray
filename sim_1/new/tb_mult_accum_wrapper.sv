a3e582033d09c2d10c9d528565141f1647ee7fe8
Hassan Al-alawi <hassanalalwi2003@gmail.com>
2025-07-03 14:39:13 +0300

    2xn and nx2 mm working

---
+++ sim_1/new/tb_mult_accum_wrapper.sv
@@ -0,0 +1,531 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/29/2025 01:33:56 PM
+// Design Name: 
+// Module Name: tb_dsp_wrapper
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+`include "PE_IF.vh"
+`include "dsp_sys_arr_pkg.vh"
+import dsp_sys_arr_pkg::*;
+
+`timescale 1ns / 1ns
+
+module tb_mult_accum_wrapper;
+
+    parameter PERIOD = 1.5;
+    parameter VEC_SIZE = 16;
+    parameter INPUT_BUFF = 1; // Number of 32 bit FP words that can be buffered at the input buffers before being processed
+                              // Would only need to support deeper queues if there is a lot of bursty behaviour in the system
+                              // Since compute latency per PE is uniform as confirmed from simulations, we expect equal rate Fetch and Store Requests
+    logic CLK = 0, nRST;
+    
+    // Test Bench Signals
+    integer testcase_num;
+    string testcase;
+    string testphase;
+    
+    integer total_testcases;
+    integer total_failed_testcases;
+    
+    logic ex_col_in_ready,ex_row_in_ready, ex_error_bit, ex_col_out_valid, ex_row_out_valid, ex_comp_done;
+    error ex_user;
+    single_float ex_accum_sum, ex_row_out_dat, ex_col_out_dat;
+    
+    single_float test;
+    shortreal inter;
+    shortreal test_row, test_col, test_sum;
+    shortreal test_vector [0:VEC_SIZE];
+    error test_user;
+    
+    // clock
+    always #(PERIOD/2) CLK++;
+
+    PE_if peif();
+    
+//    dsp_wrapper DUT(CLK, nRST, peif);
+    mult_accum_wrapper DUT(CLK, nRST, peif);
+     
+    task init_tb();
+        // Initializing Interface Input Signals
+        {peif.col_in_valid, peif.row_in_valid, peif.col_out_ready, peif.row_out_ready, peif.row_in_dat, peif.col_in_dat} = 'B0;  
+        nRST = 1'b1;  
+        
+        // Initializing TB Signals
+        {ex_col_in_ready,ex_row_in_ready, ex_error_bit, ex_col_out_valid, ex_row_out_valid, ex_accum_sum, ex_row_out_dat, ex_col_out_dat, ex_user, ex_comp_done} = 'B0;
+    endtask
+    
+    task reset_dut();
+        @(posedge CLK);
+        @(negedge CLK);
+        
+        nRST = 1'b0;
+        
+        repeat (2) @(negedge CLK);
+        
+        nRST = 1'b1;
+        
+    endtask
+    
+    task check_outputs(
+    input logic check_sum,check_row_dat, check_col_dat,
+    input logic col_in_ready, row_in_ready, error_bit, col_out_valid, row_out_valid,
+    input single_float accum_sum, row_out_dat, col_out_dat,
+    input error user,
+    input logic comp_done
+  );
+
+    total_testcases += 10;
+		
+	// Input Neighbor Ports 
+    if(peif.col_in_ready != col_in_ready) begin
+      total_failed_testcases += 1;
+      $display("At %d, Incorrect Column Input Ready signal during %s testcase in the %s test phase. \n Expected %d and got %d\n", $realtime, testcase, testphase, col_in_ready, peif.col_in_ready);
+    end
+
+    if(peif.row_in_ready != row_in_ready) begin
+      total_failed_testcases += 1;
+      $display("At %d, Incorrect Row Input Ready signal during %s testcase in the %s test phase. \n Expected %d and got %d\n", $realtime,testcase, testphase, row_in_ready, peif.row_in_ready);
+    end
+
+    // Output Neighbor Ports 
+    if((peif.col_out_valid != col_out_valid) && check_col_dat) begin
+      total_failed_testcases += 1;
+      $display("At %d, Incorrect Column Output Valid signal during %s testcase in the %s test phase. \n Expected %d and got %d\n", $realtime,testcase, testphase, col_out_valid, peif.col_out_valid);
+    end
+
+    if((peif.row_out_valid != row_out_valid) && check_row_dat) begin
+      total_failed_testcases += 1;
+      $display("At %d, Incorrect Row Output Valid signal during %s testcase in the %s test phase. \n Expected %d and got %d\n", $realtime,testcase, testphase, row_out_valid, peif.row_out_valid);
+    end
+    
+    if((peif.col_out_dat != col_out_dat) && check_col_dat) begin
+      total_failed_testcases += 1;
+      $display("At %d, Incorrect Column Output Data Bus during %s testcase in the %s test phase. \n Expected %x and got %x\n", $realtime,testcase, testphase, col_out_dat, peif.col_out_dat);
+    end
+
+    if((peif.row_out_dat != row_out_dat) && check_row_dat) begin
+      total_failed_testcases += 1;
+      $display("At %d, Incorrect Row Output Data Bus during %s testcase in the %s test phase. \n Expected %x and got %x\n", $realtime,testcase, testphase, row_out_dat, peif.row_out_dat);
+    end
+
+
+	// PE outputs
+	if((peif.accum_sum != accum_sum) && check_sum) begin
+      total_failed_testcases += 1;
+      $display("At %d, Incorrect Accumulated Sum during %s testcase in the %s test phase. \n Expected %x and got %x\n", $realtime,testcase, testphase, accum_sum, peif.accum_sum);
+    end
+    
+    if(peif.error_bit != error_bit) begin
+      total_failed_testcases += 1;
+      $display("At %d, Incorrect Error Bit during %s testcase in the %s test phase. \n Expected %d and got %d\n", $realtime,testcase, testphase, error_bit, peif.error_bit);
+    end
+    
+    if(peif.user != user) begin
+      total_failed_testcases += 1;
+      $display("At %d, Incorrect User message during %s testcase in the %s test phase. \n Expected %d and got %d\n", $realtime,testcase, testphase, user, peif.user);
+    end
+    
+    if((peif.comp_done != comp_done) && check_sum) begin
+      total_failed_testcases += 1;
+      $display("At %d, Incorrect Computation done during %s testcase in the %s test phase. \n Expected %d and got %d\n", $realtime,testcase, testphase, comp_done, peif.comp_done);
+    end
+
+	endtask
+    
+    function automatic single_float create_single_float(
+    input shortreal frac
+    );
+    
+    single_float flt;
+    int bits;
+   
+    bits = $shortrealtobits(frac);
+    flt.sign = bits[31];
+    flt.exp = bits[30:23];
+    flt.mantissa = bits[22:0];
+ 
+    return flt;
+    endfunction
+    
+    
+    
+    
+    
+    
+    task pass_inputs(
+    input succesful_pass,
+    input logic row_in_valid, col_in_valid,
+    input shortreal row_in_dat, col_in_dat
+    );
+    
+    ex_row_in_ready = 1'b0;
+    ex_col_in_ready = 1'b0;
+    ex_col_out_valid = 1'b0;
+    ex_row_out_valid = 1'b0;
+    
+    ex_col_out_dat = 'b0;
+    ex_row_out_dat = 'b0;
+    
+    if(~succesful_pass) begin
+        check_outputs(1'b0,1'b0,1'b0,ex_col_in_ready, ex_row_in_ready, ex_error_bit, ex_col_out_valid, ex_row_out_valid, 
+        ex_accum_sum, ex_row_out_dat, ex_col_out_dat, ex_user, ex_comp_done);
+    end
+    
+    else begin
+        check_outputs(1'b0,row_in_valid,col_in_valid,ex_col_in_ready, ex_row_in_ready, ex_error_bit, ex_col_out_valid, ex_row_out_valid, 
+        ex_accum_sum, ex_row_out_dat, ex_col_out_dat, ex_user, ex_comp_done);
+    end
+    
+    @(negedge CLK);
+    peif.row_in_valid = row_in_valid;
+    peif.col_in_valid = col_in_valid;
+    
+    peif.col_in_dat = create_single_float(col_in_dat);
+    peif.row_in_dat = create_single_float(row_in_dat);
+    
+    #(PERIOD*0.05);
+    ex_row_in_ready = succesful_pass & row_in_valid;
+    ex_col_in_ready = succesful_pass & col_in_valid;
+    ex_col_out_valid = 1'b0;
+    ex_row_out_valid = 1'b0;
+    
+    ex_col_out_dat = 'b0;
+    ex_row_out_dat = 'b0;
+  
+    if(~succesful_pass) begin
+        check_outputs(1'b0,1'b0,1'b0,ex_col_in_ready, ex_row_in_ready, ex_error_bit, ex_col_out_valid, ex_row_out_valid, 
+        ex_accum_sum, ex_row_out_dat, ex_col_out_dat, ex_user, ex_comp_done);
+    end
+    
+    else begin
+        check_outputs(1'b0,row_in_valid,col_in_valid,ex_col_in_ready, ex_row_in_ready, ex_error_bit, ex_col_out_valid, ex_row_out_valid, 
+        ex_accum_sum, ex_row_out_dat, ex_col_out_dat, ex_user, ex_comp_done);
+    end
+    
+    
+    @(negedge CLK);
+    
+    ex_row_in_ready = 1'b0;
+    ex_col_in_ready = 1'b0;
+   
+    ex_col_out_valid = col_in_valid;
+    ex_row_out_valid = row_in_valid;
+      
+    ex_col_out_dat = peif.col_in_dat;
+    ex_row_out_dat = peif.row_in_dat;
+    
+    if(~succesful_pass) begin
+        check_outputs(1'b0,1'b0,1'b0,ex_col_in_ready, ex_row_in_ready, ex_error_bit, ex_col_out_valid, ex_row_out_valid, 
+        ex_accum_sum, ex_row_out_dat, ex_col_out_dat, ex_user, ex_comp_done);
+    end
+    
+    else begin
+        check_outputs(1'b0,row_in_valid,col_in_valid,ex_col_in_ready, ex_row_in_ready, ex_error_bit, ex_col_out_valid, ex_row_out_valid, 
+        ex_accum_sum, ex_row_out_dat, ex_col_out_dat, ex_user, ex_comp_done);
+    end
+    
+    peif.row_in_valid = 1'B0;
+    peif.col_in_valid = 1'B0;
+    
+    peif.col_in_dat = 'B0;
+    peif.row_in_dat = 'B0;
+    
+    endtask
+    
+    
+    
+    
+    
+    task comp_check(
+    input shortreal row, col, sum,
+    input error user
+    );
+    
+    wait(peif.comp_done == 1'b1);
+    
+    inter =  row*col + sum;
+    
+    ex_row_in_ready = 1'b0;
+    ex_col_in_ready = 1'b0;
+    ex_col_out_valid = 1'b1; // Not necessarily
+    ex_row_out_valid = 1'b1;
+    ex_comp_done     = 1'b1;
+    
+    ex_error_bit = |user;
+    ex_user = user;
+    
+    ex_accum_sum   = $shortrealtobits(inter);
+    
+    check_outputs(1'b1,1'b0,1'b0,ex_col_in_ready, ex_row_in_ready, ex_error_bit, ex_col_out_valid, ex_row_out_valid, 
+    ex_accum_sum, ex_row_out_dat, ex_col_out_dat, ex_user, ex_comp_done);
+    
+    @(negedge CLK);
+    ex_row_in_ready = 1'b0;
+    ex_col_in_ready = 1'b0;
+    ex_col_out_valid = 1'b0;
+    ex_row_out_valid = 1'b0;
+    ex_comp_done     = 1'b0;
+    
+    ex_error_bit = 1'b0;
+    ex_user = 'b0;
+    
+    ex_accum_sum   = 'b0;
+    
+    endtask
+    
+    
+    
+    
+    
+    
+    task operand_request(
+    input logic col_out_ready, row_out_ready,
+    input shortreal col_out_dat, row_out_dat
+    );
+    
+    @(negedge CLK);
+    ex_col_out_valid = 1'b1;
+    ex_row_out_valid = 1'b1;
+    ex_col_out_dat = create_single_float(col_out_dat);
+    ex_row_out_dat = create_single_float(row_out_dat);
+    
+    peif.col_out_ready = col_out_ready;
+    peif.row_out_ready = row_out_ready;
+    
+    check_outputs(1'b0,1'b1,1'b1,ex_col_in_ready, ex_row_in_ready, ex_error_bit, ex_col_out_valid, ex_row_out_valid, 
+    ex_accum_sum, ex_row_out_dat, ex_col_out_dat, ex_user, ex_comp_done);
+    
+    @(negedge CLK);
+    ex_col_out_valid = 1'b0;
+    ex_row_out_valid = 1'b0;    
+    peif.col_out_ready = 'b0;
+    peif.row_out_ready = 'b0;
+    
+    ex_col_out_dat = 'b0;
+    ex_row_out_dat = 'b0;
+    
+    check_outputs(1'b0,1'b1,1'b1,ex_col_in_ready, ex_row_in_ready, ex_error_bit, ex_col_out_valid, ex_row_out_valid, 
+    ex_accum_sum, ex_row_out_dat, ex_col_out_dat, ex_user, ex_comp_done);
+    
+    #(PERIOD*0.05);
+    endtask
+    
+    
+    initial begin
+        testcase_num = 0;
+        testcase = "Initialization";
+        testphase = "Inititization";
+        
+        total_testcases = 0;
+        total_failed_testcases = 0;
+        
+        test_row = 0;
+        test_col = 0;
+        test_sum = 0;
+        
+        test_user = 'b0;
+        
+        init_tb();
+        reset_dut();
+        
+        repeat (5) @(posedge CLK);
+        
+        // DUT ready to process
+        // Inputs port valid
+        // Output ports ready
+        testphase = "Basic Operation";
+        
+        // First pass inputs to both ports
+        testcase = "Passing Operands";
+        test_row = 2.0;
+        test_col = 2.5;
+        pass_inputs(1'b1,1'b1,1'b1,test_row,test_col);
+       
+        // Verify that computation has been completed succesfully
+        testcase = "Checking Computation";
+        test_sum = 0;
+        comp_check(test_row, test_col, test_sum, test_user);
+        
+        test_sum += test_col*test_row;
+        
+        // Dual-Operand request
+        @(posedge CLK);              
+        testcase = "Operand Request";
+        operand_request(1'b1,1'b1,test_col,test_row);
+        
+        
+        // Stream Computation (Check for Accumulated Sum)
+        testphase = "Stream Computation";
+        reset_dut();
+        repeat (5) @(posedge CLK);
+        
+        testcase = "Fill, Compute and Empty";
+        
+        test_sum = 0;
+        
+        for(int j=0; j<VEC_SIZE; j++) begin
+            test_vector[j] = j+1;
+        end
+        
+        for (int i=0; i < 6; i++) begin
+            test_row = test_vector[i];
+            test_col = test_vector[i];
+            pass_inputs(1'b1,1'b1,1'b1,test_row,test_col);
+            
+            comp_check(test_row, test_col, test_sum, test_user);
+        
+            test_sum += test_col*test_row;
+            
+            @(posedge CLK);              
+            operand_request(1'b1,1'b1,test_col,test_row);
+        end 
+        
+        testcase = "Fill, and Empty, then compute"; // Fill the port A and B input buffers, by continously reading operands while PE is computing
+        reset_dut();
+        repeat (5) @(posedge CLK);
+        
+        test_sum = 0;
+        
+        for (int i=0; i < INPUT_BUFF+1; i++) begin // Through tests we know that buffers can only handle 1 unprocessed operand/s  to be queued
+            test_row = test_vector[i];             // Since we are passing both col and row operands at once, they will be buffered and then popped allowing for INPUT_BUFF + 1 operands to be passed in
+            test_col = test_vector[i];
+            pass_inputs(1'b1,1'b1,1'b1,test_row,test_col);
+                         
+            operand_request(1'b1,1'b1,test_col,test_row);
+        end
+        
+        
+        for (int i=0; i < INPUT_BUFF+1; i++) begin
+            test_row = test_vector[i];
+            test_col = test_vector[i];
+            
+            comp_check(test_row, test_col, test_sum, test_user);
+        
+            test_sum += test_col*test_row;
+            
+            repeat(2) @(posedge CLK);  
+        end
+        
+        // Check that DUT doesnt accept row/column input if row/column operand has not been read
+        testphase = "Check Operand Passing";
+        reset_dut();
+        repeat (5) @(posedge CLK);
+        
+        // Check only row operand passing 
+        testcase = "Row input Passing";
+        for (int i=0; i < INPUT_BUFF+1; i++) begin
+            if(i == INPUT_BUFF) begin
+                test_row = test_vector[i];
+                test_col = test_vector[i];
+                pass_inputs(1'b0, 1'b1,1'b0,test_row,test_col); 
+            end
+            
+            else begin
+                test_row = test_vector[i];          
+                test_col = test_vector[i];
+                pass_inputs(1'b1, 1'b1,1'b0,test_row,test_col);
+            end
+        end
+        
+        // Check only column operand passing
+        reset_dut();
+        repeat (5) @(posedge CLK);
+        testcase = "Column input Passing";
+        for (int i=0; i < INPUT_BUFF+1; i++) begin
+            if(i == INPUT_BUFF) begin
+                test_row = test_vector[i];
+                test_col = test_vector[i];
+                pass_inputs(1'b0, 1'b0,1'b1,test_row,test_col); 
+            end
+            
+            else begin
+                test_row = test_vector[i];          
+                test_col = test_vector[i];
+                pass_inputs(1'b1, 1'b0,1'b1,test_row,test_col);
+            end
+        end
+        
+        // Check for correct compute order
+        testphase = "Check Computed Order";
+        reset_dut();
+        repeat (5) @(posedge CLK);
+        
+        testcase = "Filling Row Input Buffer";
+        for (int i=0; i < INPUT_BUFF; i++) begin // Fill Row Buffer
+            test_row = test_vector[i];          
+            test_col = test_vector[i];
+            pass_inputs(1'b1, 1'b1,1'b0,test_row,test_col);
+        end
+        
+        testcase = "Filling Column Input Buffer";
+        for (int i=0; i < INPUT_BUFF; i++) begin // Fill Column Buffer
+            test_row = test_vector[i];          
+            test_col = test_vector[i];
+            pass_inputs(1'b1, 1'b0,1'b1,test_row,test_col);
+        end
+        
+        
+        testcase = "Checking Outputs to ensure they match computed order";
+        test_sum = 0;
+        
+        for (int i=0; i < INPUT_BUFF; i++) begin
+            test_row = test_vector[i];
+            test_col = test_vector[i];
+            
+            comp_check(test_row, test_col, test_sum, test_user);
+        
+            test_sum += test_col*test_row;
+            
+            repeat(2) @(posedge CLK);  
+        end
+        
+        testphase = "Error Checking";
+        reset_dut();
+        repeat (5) @(posedge CLK);
+        // Check for overflow 
+        testcase = "Overflow check";
+        test_row = $bitstoshortreal({1'b0,8'hFE,23'b11101110011010110000101});
+        test_col = $bitstoshortreal({1'b0,8'h82,23'b01000000000000000000000});
+        pass_inputs(1'b1,1'b1,1'b1,test_row,test_col);
+      
+        test_sum = 0;
+        test_user.overflow = 1'b1;
+        test_user.underflow = 1'b0;
+        
+        comp_check(test_row, test_col, test_sum, test_user);
+        
+        // Check for undeflow
+        reset_dut();
+        repeat (5) @(posedge CLK);
+        testcase = "Underflow check";
+        test_row = $bitstoshortreal({1'b0,8'h01,23'b10111101011100011111111});
+        test_col = $bitstoshortreal({1'b0,8'h01,23'b11000110101110011011101});
+        pass_inputs(1'b1,1'b1,1'b1,test_row,test_col);
+      
+        test_sum = 0;
+        test_user.overflow = 1'b0;
+        test_user.underflow = 1'b1;
+        
+        comp_check(test_row, test_col, test_sum, test_user);
+                 
+        $display("Testcases Failed/Total Testcases: %d/%d",total_failed_testcases, total_testcases);
+        $finish();
+    
+    end
+
+endmodule
---
+++ sources_1/new/sys_array.sv
@@ -0,0 +1,178 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/01/2025 04:57:40 PM
+// Design Name: 
+// Module Name: sys_array
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: Systolic Array that multiplies A mxn by B nxk matrix to produce C mxk
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+`include "PE_IF.vh"
+`include "dsp_sys_arr_pkg.vh"
+import dsp_sys_arr_pkg::*;
+
+module sys_array #(parameter m = 2, parameter n = 2, parameter k = 2)(
+input logic clk, nrst,
+output logic comp_done, error,
+output single_float r1c1_out, r1c2_out, r2c1_out, r2c2_out);
+
+parameter N = 3;
+
+PE_if peifr1c1();
+PE_if peifr2c1();
+PE_if peifr1c2();
+PE_if peifr2c2();
+
+single_float [N-1:0] a_r1, a_r2, b_c1, b_c2, n_a_r1, n_a_r2, n_b_c1, n_b_c2;
+
+logic ar1_strt, bc1_strt, n_ar1_strt, n_bc1_strt;
+logic [3:0] r1c1_ctr, n_r1c1_ctr, r2c2_ctr, n_r2c2_ctr;
+
+assign r1c1_out = peifr1c1.accum_sum;
+assign r1c2_out = peifr1c2.accum_sum;
+assign r2c1_out = peifr2c1.accum_sum;
+assign r2c2_out = peifr2c2.accum_sum;
+
+
+dsp_wrapper r1c1(clk, nrst, peifr1c1);
+dsp_wrapper r1c2(clk, nrst, peifr1c2);
+dsp_wrapper r2c1(clk, nrst, peifr2c1);
+dsp_wrapper r2c2(clk, nrst, peifr2c2); 
+
+always_ff @(posedge clk, negedge nrst) begin
+
+    if(~nrst) begin
+//        for (logic i=0; i<N; i++) begin
+//            a_r1[i] <= $shortrealtobits(1.0 + i);
+//            a_r2[i] <= $shortrealtobits(1.0*N + i + 1);
+//            b_c1[i] <= $shortrealtobits(1.0 + i*2);;
+//            b_c2[i] <= $shortrealtobits(2 + i*2);
+//        end
+        a_r1 <= {$shortrealtobits(1.0),$shortrealtobits(2.0),$shortrealtobits(3.0)};
+        a_r2 <= {$shortrealtobits(4.0),$shortrealtobits(5.0),$shortrealtobits(6.0)};
+        b_c1 <= {$shortrealtobits(1.0),$shortrealtobits(2.0),$shortrealtobits(3.0)};
+        b_c2 <= {$shortrealtobits(4.0),$shortrealtobits(5.0),$shortrealtobits(6.0)};
+        
+        ar1_strt <= 1'b1;
+        bc1_strt <= 1'b1;
+        
+        r1c1_ctr <= '0;
+        r2c2_ctr <= '0;
+    end
+    
+    else begin
+        a_r1 <= n_a_r1;
+        a_r2 <= n_a_r2;
+        b_c1 <= n_b_c1;
+        b_c2 <= n_b_c2;
+        
+        ar1_strt <= n_ar1_strt;
+        bc1_strt <= n_bc1_strt;
+        
+        r1c1_ctr <= n_r1c1_ctr;
+        r2c2_ctr <= n_r2c2_ctr;
+    end
+
+end
+
+// Interface Logic
+always_comb begin
+    // Edge PE connections
+    peifr1c1.row_in_valid = 1'b0;
+    peifr1c1.col_in_valid = 1'b0;
+    peifr1c2.col_in_valid = 1'b0;
+    peifr2c1.row_in_valid = 1'b0;
+    
+    // Edge PEs are have output port ready signals that are connected to any PEs tied to 1
+    peifr1c2.row_out_ready = 1'b1;
+    peifr2c2.row_out_ready = 1'b1;
+    peifr2c2.col_out_ready = 1'b1;
+    peifr2c1.col_out_ready = 1'b1;
+    
+    // Edge PEs whos data inputs are not connected to any PEs are tied to wrapper regs
+    peifr1c1.row_in_dat = a_r1[0];
+    peifr1c1.col_in_dat = b_c1[0];
+    peifr2c1.row_in_dat = a_r2[0];
+    peifr1c2.col_in_dat = b_c2[0];
+    
+    comp_done = peifr1c1.comp_done & peifr1c2.comp_done & peifr2c1.comp_done & peifr2c2.comp_done;
+    error = peifr1c1.error_bit & peifr1c2.error_bit & peifr2c1.error_bit & peifr2c2.error_bit;
+    
+    if(r1c1_ctr != N) begin
+        peifr1c1.row_in_valid = 1'b1;
+        peifr1c1.col_in_valid = 1'b1;
+    end
+    
+    if(r2c2_ctr != N) begin
+        peifr2c1.row_in_valid = ar1_strt;
+        peifr1c2.col_in_valid = bc1_strt;
+    end
+    
+    // Internal PE connections
+    // R1C1<->R1C2
+    peifr1c2.row_in_dat     = peifr1c1.row_out_dat;
+    peifr1c2.row_in_valid   = peifr1c1.row_out_valid;
+    peifr1c1.row_out_ready  = peifr1c2.row_in_ready;
+    
+    // R1C1<->R2C1 
+    peifr2c1.col_in_dat     = peifr1c1.col_out_dat;
+    peifr2c1.col_in_valid   = peifr1c1.col_out_valid;
+    peifr1c1.col_out_ready  = peifr2c1.col_in_ready;
+    
+    // R2C2<->R1C2
+    peifr2c2.col_in_dat     = peifr1c2.col_out_dat;
+    peifr2c2.col_in_valid   = peifr1c2.col_out_valid;
+    peifr1c2.col_out_ready  = peifr2c2.col_in_ready;
+    
+    // R2C2<->R2C1
+    peifr2c2.row_in_dat     = peifr2c1.row_out_dat;
+    peifr2c2.row_in_valid   = peifr2c1.row_out_valid;
+    peifr2c1.row_out_ready  = peifr2c2.row_in_ready;
+end
+
+// Shift Register and Counter Logic Logic
+always_comb begin
+    n_a_r1 = a_r1;
+    n_a_r2 = a_r2;
+    n_b_c1 = b_c1;
+    n_b_c2 = b_c2;
+    
+    n_ar1_strt = ar1_strt;
+    n_bc1_strt = bc1_strt;
+    
+    // Counter used so that shift register stops shifting in data after all its operands are in the systolic array
+    // Essential so that valid signal goes low and comp_done reg doesnt get inccorectly cleared.
+    n_r1c1_ctr = r1c1_ctr;
+    n_r2c2_ctr = r2c2_ctr;
+    
+    // Logic to control when subsequent rows begin shifting in their inputs
+//    if(n_r1c1_ctr == 1'd1) begin
+//        n_ar1_strt = 1'b1;
+//        n_bc1_strt = 1'b1;
+//    end
+    
+    if(peifr1c1.row_in_valid & peifr1c1.row_in_ready & peifr1c1.col_in_valid & peifr1c1.col_in_ready)  begin
+        n_a_r1 = a_r1 >> SNGL_FLT_SIZE;
+        n_b_c1 = b_c1 >> SNGL_FLT_SIZE;
+        n_r1c1_ctr = r1c1_ctr + 'd1;
+    end    
+    
+    if(peifr2c1.row_in_valid & peifr2c1.row_in_ready & peifr1c2.col_in_valid & peifr1c2.col_in_ready)  begin
+        n_a_r2 = a_r2 >> SNGL_FLT_SIZE;
+        n_b_c2 = b_c2 >> SNGL_FLT_SIZE;
+        n_r2c2_ctr = r2c2_ctr + 'd1;
+    end  
+end
+   
+endmodule
